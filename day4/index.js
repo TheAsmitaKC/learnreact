c4d9819b2408ef215e1b3ee4ea4ec9233a3b557b
theasmitakc <theasmitakc@gmail.com>
2025-05-24 23:14:20 +0545

    initial commit

---
+++ day4/index.js
@@ -0,0 +1,33 @@
+//----------Day 4 React---
+//DOM Manipulation
+// const para = document.getElementById("demo");
+// console.log(para.textContent);
+
+// function changetext() {
+//   document.getElementById("text").textContent = "Update";
+// }
+
+// document.getElementById("btn").addEventListener("click", () => {
+//   alert("Button clicked!");
+// });
+// document.getElementById("btn").addEventListener("click", () => {
+//   alert("Button clciked");
+// });
+// document.getElementById("btn").addEventListener("mouseover", () => {
+//   console.log("hovered");
+// });
+
+//------Event Handling------------
+// document.addEventListener("keydown", (e) => {
+//   console.log(`key pressed ${e}`);
+// });
+// document.querySelector("form").addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   alert("Successful");
+// });
+// document.querySelector("form").addEventListener("dblclick", () => {
+//   console.log("double clicked");
+// });
+// document.querySelector("form").addEventListener("input", (e) => {
+//   console.log(e.target.value);
+// });
